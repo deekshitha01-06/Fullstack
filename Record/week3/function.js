78501b6d011f259fd654f3c3636ec9a1b7ccf730
Deekshitha Sasupilli <sasupillideekshitha@gmail.com>
2026-08-31 22:54:06 +0530

    Add files via upload

---
+++ Record/week3/function.js
@@ -0,0 +1,21 @@
+let shoppingList = ["Milk", "Bread", "Eggs"];
+ 
+function addItem(newItem) {
+  shoppingList.push(newItem);
+}
+ 
+console.log("Initial List:", shoppingList);
+addItem("Bananas");
+addItem("Cheese");
+console.log("Updated List:", shoppingList);
+ 
+const food=["pizza","burger","kfc","frenchfries"];
+console.log(food);
+food.push("tandori");
+food.unshift("biriyani");
+console.log("modified array is: ",food);
+food.shift();
+console.log(" array is: ",food);
+ 
+const extend=[...food,"chicken curry","pulav"];
+console.log(extend);
